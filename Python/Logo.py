e9aaacc950c9ba6eb530f501141ccb54e9035f3b
kelsey21 <jiangyaohan@gmail.com>
2017-02-25 01:44:33 -0600

    Update Logo.py

--- Python/Logo.py
+++ Python/Logo.py
@@ -3,17 +3,7 @@
 
 #  Description: A program with Superman logo
 
-#  Student Name: Yaohan Jiang
 
-#  Student UT EID: yj3948
-
-#  Course Name: CS 303E
-
-#  Unique Number: 51200
-
-#  Date Created: Sep.5
-
-#  Date Last Modified:
 
 def main():
     print()
